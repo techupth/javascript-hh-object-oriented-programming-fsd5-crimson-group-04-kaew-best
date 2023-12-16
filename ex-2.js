a2b7d67f54c3812353be2b386621798b121c6e04
kaew <rajitkaew2285@gmail.com>
2023-12-16 12:17:53 +0700

    feat(ex1-3): ex complete

--- ex-2.js
+++ ex-2.js
@@ -1,5 +1,30 @@
-class Notification { }
+class Notification {
+  constructor(notificationId, createdTime, content, receiver) {
+    this.notificationId = notificationId;
+    this.createdTime = createdTime;
+    this.content = content;
+    this.receiver = receiver;
+  }
+  send() {
+    console.log(`Notification has been sent to ${this.receiver}`);
+  }
+}
 
-class EmailNotification { }
+class EmailNotification extends Notification {
+  constructor(notificationId, createdTime, content, receiver) {
+    super(notificationId, createdTime, content);
+    this.receiver = receiver;
+  }
+}
 
-class SMSNotification { }
+class SMSNotification extends Notification {
+  constructor(notificationId, createdTime, content, receiver) {
+    super(notificationId, createdTime, content);
+    this.receiver = receiver;
+  }
+}
+const receiverEmail = new EmailNotification("1", "10.0", "email", "Kaew");
+const myPhone = new SMSNotification("1", "10.0", "email", "0805086115");
+
+receiverEmail.send();
+myPhone.send();
